e3d303e591211be93d02ba17d12caff7e8207740
SyrineZahar <cyrine.zahaar@gmail.com>
2024-08-07 12:36:40 +0100

    home_page

--- front-end/lib/views/client/home_page.dart
+++ front-end/lib/views/client/home_page.dart
@@ -218,18 +218,13 @@ class HomePageR extends StatefulWidget {
 class _HomePageRState extends State<HomePageR> {
   int _currentIndex = 0;
 
-  @override
-  void initState() {
-    super.initState();
-    _pages.addAll([
+
+  final List<Widget> _pages = [      
       HomeScreen(),
       ProfilePage(),
       FavorisPage(),
       ChartPage(),
-    ]);
-  }
-
-  final List<Widget> _pages = [];
+    ];
 
   void _onItemTapped(int index) {
     setState(() {
@@ -409,88 +404,352 @@ class _HomeScreenState extends State<HomeScreen> {
                         ],
                       ),
                       SizedBox(height: 10),
-                      Text(
-                        'Bonjour',
-                        style: TextStyle(
-                          fontSize: 25,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Text(
-                        'Commandez tous les produits bio dont vous avez besoin ici',
-                        style: TextStyle(
-                          fontSize: 12,
-                        ),
-                      ),
                     ],
                   ),
                 ),
               ),
             ),
-            Container(
-              height: 150,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 5,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        width: 150,
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).colorScheme.primary,
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                      ),
-                    );
-                  },
+            SizedBox(height: 10),
+             Container(
+          padding: EdgeInsets.all(10),
+            decoration: BoxDecoration(
+             color: Theme.of(context).colorScheme.onError,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 5,
+                  offset: Offset(0, 3),
                 ),
-              ),
+              ],
             ),
-            Container(
-              height: 200,
-              color: Colors.amber,
-              child: CarouselSlider(
-                options: CarouselOptions(
-                  height: 200.0,
-                  enlargeCenterPage: true,
-                  autoPlay: true,
-                  aspectRatio: 16 / 9,
-                  autoPlayCurve: Curves.fastOutSlowIn,
-                  enableInfiniteScroll: true,
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                  viewportFraction: 0.8,
-                ),
-                items: [
-                  'https://cdn.pixabay.com/photo/2021/03/22/12/10/woman-6113940_1280.jpg',
-                  'https://cdn.pixabay.com/photo/2017/09/04/18/51/vegetables-2715740_1280.jpg',
-                  'https://cdn.pixabay.com/photo/2015/03/26/09/54/vegetables-690022_1280.jpg'
-                ].map((i) {
-                  return Builder(
-                    builder: (BuildContext context) {
-                      return Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(
-                          color: Colors.amber,
-                        ),
-                        child: Image.network(
-                          i,
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    },
-                  );
-                }).toList(),
-              ),
-            ),
-          ],
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+      width: 370, 
+      child: TextField(
+        decoration: InputDecoration(
+          prefixIcon: IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              // Handle search button
+            },
+          ),
+          filled: true,
+          fillColor: Color.fromARGB(255, 214, 214, 214),
+          hintText: 'Recherchez Des Plats Où Des Restaurants..',  
+          hintStyle: TextStyle(fontSize: 12,color: Theme.of(context).colorScheme.tertiary, ), 
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide.none,
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         ),
       ),
-    );
-  }
+    ),SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 37,
+                          height: 37,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Nos Categories',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+CarouselSlider(
+          options: CarouselOptions(
+            height: 40, // Adjust height as needed
+            initialPage: 0,
+            viewportFraction: 0.3, // Adjust to control space between items
+            enlargeCenterPage: false, // Ensure items are not enlarged
+            enableInfiniteScroll: false,
+            aspectRatio: 2.0,
+            scrollDirection: Axis.horizontal, // Ensure horizontal scrolling
+            reverse: false, // Ensure items scroll from left to right
+          ),
+          items: [
+            'All',
+            'Petit Dej',
+            'Plats',
+            'Smoothies',
+          ].map((item) => Container(
+            margin: EdgeInsets.symmetric(horizontal: 4), // Reduce space between items
+            child: Container(
+              decoration: BoxDecoration(
+                color: item == 'All' ? Colors.orange : Color.fromARGB(255, 217, 215, 215),
+                borderRadius: BorderRadius.circular(25),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 1,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Center(
+                child: Text(
+                  item,
+                  style: TextStyle(
+                    color: Theme.of(context).colorScheme.secondary,
+                    fontSize: 14, // Adjust font size if needed
+                  ),
+                ),
+              ),
+            ),
+          )).toList(),
+        ),
+        SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 37,
+                          height: 37,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          "Special Aujourd'hui",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                CarouselSlider(
+                  options: CarouselOptions(
+                    height: 250,
+                    enlargeCenterPage: true,
+                    autoPlay: true,
+                    enableInfiniteScroll: true,
+                  ),
+                  items: [
+                    Container(
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 5),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(15),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                spreadRadius: 1,
+                                blurRadius: 5,
+                                offset: Offset(0, 3),
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(20),
+                                    topRight: Radius.circular(20),
+                                  ),
+                                  child: Image.network(
+                                    'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Lula_kebab_2.jpg/640px-Lula_kebab_2.jpg',
+                                    width: 300, // Adjust width as needed
+                                    height: 200, // Adjust height as needed
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 10),
+                                child: Text(
+                                  'Kebab Mechhwi',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Text(
+                                'Prix : 250 Dt',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              Text(
+                                'offre speciale sur le kebeb',
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 12,
+                                ),
+                              ),
+                              SizedBox(height: 8),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ].map((item) => Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 4.0),
+                    child: item,
+                  )).toList(),
+                ),
+                SizedBox(height: 20),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          width: 37,
+                          height: 37,
+                          decoration: BoxDecoration(
+                            color: Colors.orange,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(
+                            Icons.star,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Text(
+                          'Restaurants Ouverts',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                    InkWell(
+                      onTap: () {
+                        // Handle click
+                      },
+                      child: Text(
+                        'voir plus',
+                        style: TextStyle(
+                          color: Colors.grey,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Container(
+      margin: EdgeInsets.symmetric(vertical: 10), // Add margin to separate items
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 5),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 5,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch, // Stretch content to fill width
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image.network(
+                  'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Lula_kebab_2.jpg/640px-Lula_kebab_2.jpg',
+                  width: double.infinity, // Use full width of container
+                  height: 200, // Fixed height
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start, // Align text to the start
+                  children: [ 
+                    Text(
+                      'Restaurant ',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    SizedBox(height: 5), // Space between text lines
+                    Text(
+                      'Prix : 250 Dt',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14, // Slightly larger font size for price
+                      ),
+                    ),
+                    SizedBox(height: 5), // Space between text lines
+                    Text(
+                      'Offre spéciale sur le kebab',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 12,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    )  
+              ],
+            ),
+            
+          ),
+        ],
+      ),
+    ),
+    )
+  ;}
 }
-
-
